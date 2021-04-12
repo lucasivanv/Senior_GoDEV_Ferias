d7aa4568eeb45d8c3c0f697ab89e55721d1f4934
SENIOR <SENIOR@proway.treina>
2021-04-12 15:58:00 -0300

    Criação dos métodos: verificarSePossuiSaldo, informarSaldo

--- src/br/com/proway/senior/ferias/Main.java
+++ src/br/com/proway/senior/ferias/Main.java
@@ -1,5 +1,42 @@
 package br.com.proway.senior.ferias;
 
 public class Main {
-	String usuario = "Janaina";
+	public static void main(String[] args) {
+		/*O sistema recebe o colaborador e verifica se 
+		ele possui saldo de férias positivo.*/
+		System.out.println(informarSaldo(1));
+		
+	}
+	
+	static int[] saldoDeFerias = {20, 30, 15, 10};
+
+	public static boolean verificarSePossuiSaldoPositivo(int idColaborador) {
+		int saldo = consultarSaldo(idColaborador);
+		boolean podeTirarFerias = saldo > 0 ? true : false;
+
+		return podeTirarFerias;
+	}
+	
+	public static int consultarSaldo(int idColaborador) {
+		int saldo = saldoDeFerias[idColaborador];
+		return saldo;
+	}
+
+	public static String informarSaldo(int idColaborador) {
+		String mensagem;
+
+		if(verificarSePossuiSaldoPositivo(idColaborador)) {
+			mensagem = "Saldo positivo: " + consultarSaldo(idColaborador);
+		}
+		else {
+			mensagem = "Não existe saldo de férias disponível para o usuário informado.";
+		}
+		return mensagem;
+	}
+	
+	
+	
+	
+	
+	
 }
