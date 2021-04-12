bad094fcb17b418f20c4159088103a4893e6adee
Lucas Ivan <lucas.ivan.medeiros@gmail.com>
2021-04-12 17:28:00 -0300

    Sistema verifica entre data inicio e data chamado

--- src/br/com/proway/senior/ferias/Main.java
+++ src/br/com/proway/senior/ferias/Main.java
@@ -1,36 +1,51 @@
 package br.com.proway.senior.ferias;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
 
 	static void registraDatasFerias(String respostaTipoFerias){
 
-		if (respostaTipoFerias == "total") {			
+		if (verificarDataDeChamado()) {			
+			if (respostaTipoFerias == "total") {			
 
-			LocalDateTime dataInicioFerias = getDataInicioFerias();
+				LocalDateTime dataInicioFerias = getDataInicioFerias();
 
-			LocalDateTime dataFimFerias = dataInicioFerias.plusDays(30);
-			registraDataInicioFerias(dataInicioFerias);
-			registraDataFimFerias(dataFimFerias);
+				LocalDateTime dataFimFerias = dataInicioFerias.plusDays(30);
+				registraDataInicioFerias(dataInicioFerias);
+				registraDataFimFerias(dataFimFerias);
 
-		} else if(respostaTipoFerias == "parcial") {
-			LocalDateTime dataInicioFerias = getDataInicioFerias();
-			LocalDateTime dataFimFerias = getDataFimFerias();
+			} else if(respostaTipoFerias == "parcial") {
+				
+				if (verificarIntervaloFeriasParcial()) {
+					LocalDateTime dataInicioFerias = getDataInicioFerias();
+					LocalDateTime dataFimFerias = getDataFimFerias();
 
-			registraDataInicioFerias(dataInicioFerias);
-			registraDataFimFerias(dataFimFerias);
-		}
+					registraDataInicioFerias(dataInicioFerias);
+					registraDataFimFerias(dataFimFerias);
+				} else {
+					System.out.println("O intervalo solicitado é inválido (superior a 20 dias)");
+				}	
+			}			
+		} else {
+			System.out.println("A data da solicitação é inferior a 10 dias deste chamado");
+		}		
 	}
-
+	
 	static LocalDateTime getDataInicioFerias() {
-		LocalDateTime respostaDataInicioFerias = LocalDateTime.of(2021, 5, 15, 0, 0);
+		LocalDateTime respostaDataInicioFerias = LocalDateTime.of(2021, 3, 23, 0, 0);
 		return respostaDataInicioFerias;
 	}
 
 	static LocalDateTime getDataFimFerias() {
-		LocalDateTime respostaDataFimFerias = LocalDateTime.of(2021, 5, 16, 0, 0);
+		LocalDateTime respostaDataFimFerias = LocalDateTime.of(2021, 4, 1, 0, 0);
 		return respostaDataFimFerias;
+	}
+
+	public static LocalDateTime getDataDeChamado() {
+		LocalDateTime dataChamado = LocalDateTime.of(2021, 3, 12, 0, 0);
+		return dataChamado;			
 	}
 
 	static void registraDataInicioFerias(LocalDateTime respostaDataInicioFerias) {
@@ -43,8 +58,22 @@ public class Main {
 		System.out.println(dataFim);
 	}
 
+	public static boolean verificarDataDeChamado() {
+		LocalDateTime dataChamado = getDataDeChamado();
+		LocalDateTime dataInicio = getDataInicioFerias();
+		boolean intervaloSuperior10dias = dataChamado.until(dataInicio, ChronoUnit.DAYS) > 10? true : false;
+		return intervaloSuperior10dias;
+	}
+
+	public static boolean verificarIntervaloFeriasParcial() {
+		LocalDateTime dataFim = getDataFimFerias();
+		LocalDateTime dataInicio = getDataInicioFerias();
+		boolean intervaloInferior20dias = dataInicio.until(dataFim, ChronoUnit.DAYS) < 20? true : false;
+		return intervaloInferior20dias;
+	}	
+	
 	public static void main(String[] args) {
-		String respostaTipoFerias = "total";
+		String respostaTipoFerias = "parcial";
 		registraDatasFerias(respostaTipoFerias);		
 	}
 }
