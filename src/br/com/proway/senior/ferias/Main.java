dd72ac6fbf7a190d321d96cfe67d2b4c22d5bfb7
SENIOR <SENIOR@proway.treina>
2021-04-12 15:43:11 -0300

    Criação do método que retorna a quantidade de saldo

--- src/br/com/proway/senior/ferias/Main.java
+++ src/br/com/proway/senior/ferias/Main.java
@@ -1,5 +1,19 @@
 package br.com.proway.senior.ferias;
 
 public class Main {
+	static int[] saldoDeFerias = { 30, 25, 40, 10, 5 };	
+	
+	public static void main(String[] args) {
+		int saldo = consultarSaldo(4);
+		System.out.println("O seu saldo é " + saldo);
+	}
+	// Se sim, o sistema retorna quantidade de saldo disponível de férias do colaborador.
 
+
+
+	public static int consultarSaldo(int idColaborador) {
+		int saldo = saldoDeFerias[idColaborador];
+		return saldo;
+	}
+	
 }
