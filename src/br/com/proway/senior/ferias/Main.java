de9cf9feebf0c7ee725c934c1b31199df94835d2
Lucas Ivan <lucas.ivan.medeiros@gmail.com>
2021-04-12 15:50:14 -0300

    Sistema recebe tipo de ferias

--- src/br/com/proway/senior/ferias/Main.java
+++ src/br/com/proway/senior/ferias/Main.java
@@ -1,5 +1,51 @@
 package br.com.proway.senior.ferias;
 
+import java.time.LocalDateTime;
+
 public class Main {
 
+	static void registraDatasFerias(String respostaTipoFerias){
+
+		if (respostaTipoFerias == "total") {			
+
+			LocalDateTime dataInicioFerias = getDataInicioFerias();
+
+			LocalDateTime dataFimFerias = dataInicioFerias.plusDays(30);
+			registraDataInicioFerias(dataInicioFerias);
+			registraDataFimFerias(dataFimFerias);
+
+		} else if(respostaTipoFerias == "parcial") {
+			LocalDateTime dataInicioFerias = getDataInicioFerias();
+			LocalDateTime dataFimFerias = getDataFimFerias();
+
+			registraDataInicioFerias(dataInicioFerias);
+			registraDataFimFerias(dataFimFerias);
+		}
+	}
+
+	static LocalDateTime getDataInicioFerias() {
+		LocalDateTime respostaDataInicioFerias = LocalDateTime.of(2021, 5, 15, 0, 0);
+		return respostaDataInicioFerias;
+	}
+
+	static LocalDateTime getDataFimFerias() {
+		LocalDateTime respostaDataFimFerias = LocalDateTime.of(2021, 5, 16, 0, 0);
+		return respostaDataFimFerias;
+	}
+
+	static void registraDataInicioFerias(LocalDateTime respostaDataInicioFerias) {
+		LocalDateTime dataInicio = respostaDataInicioFerias;
+		System.out.println(dataInicio);
+	}
+
+	static void registraDataFimFerias(LocalDateTime respostaDataFimFerias) {
+		LocalDateTime dataFim = respostaDataFimFerias;
+		System.out.println(dataFim);
+	}
+
+	public static void main(String[] args) {
+		String respostaTipoFerias = "total";
+		registraDatasFerias(respostaTipoFerias);		
+	}
 }
+
