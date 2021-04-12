8aed134d4b36a6c81d73ddaf781a4aba40436a2b
Buno Marques <brunoliveira.marques@gmail.com>
2021-04-12 17:43:52 -0300

    inicio de consulta de colaborador

--- src/br/com/proway/senior/ferias/Main.java
+++ src/br/com/proway/senior/ferias/Main.java
@@ -1,11 +1,11 @@
 package br.com.proway.senior.ferias;
 
 public class Main {
-	static int[] saldoDeFerias = { 30, 25, 40, 10, 5 };	
-	
+	static int[] saldoDeFerias = { 30, 25, 40, 10, 5 };
+
 	public static void main(String[] args) {
-		
-		//O método retorna quantidade de saldo disponível de férias do colaborador.
+
+		// O método retorna quantidade de saldo disponível de férias do colaborador.
 		int saldo = consultarSaldo(4);
 		System.out.println("O seu saldo é " + saldo);
 	}
@@ -13,8 +13,11 @@ public class Main {
 	public static int consultarSaldo(int idColaborador) {
 		int saldo = saldoDeFerias[idColaborador];
 		return saldo;
+
 	}
 	
-	
-	
+	//O sistema recebe o usuário e verifica se este possui colaboradores em sua equipe
+	public static int consultarColaboraoresDaEquipe(int idColaborador) {
+		
+	}
 }
