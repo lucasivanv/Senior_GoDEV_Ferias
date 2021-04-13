91cb88692d35f2accb1cb25575e2a351d437b763
Lucas Ivan <lucas.ivan.medeiros@gmail.com>
2021-04-13 17:03:02 -0300

    Atualizacao do projeto

---
+++ src/test/java/br/com/proway/senior/controledeferias/MainTest.java
@@ -0,0 +1,35 @@
+package br.com.proway.senior.controledeferias;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void verificaSeIntervaloEntreDataSolicitacaoEDataInicioFeriasEhMaiorQue10Dias() {
+		boolean intervaloEntreSolicitacaoEInicio = Main.verificarDataSolicitacao(LocalDateTime.now().plusDays(20));
+		assertTrue(intervaloEntreSolicitacaoEInicio);
+	}
+	
+	@Test
+	public void verificaSeIntervaloEntreDataSolicitacaoEDataInicioFeriasEhMenorQue10Dias() {
+		boolean intervaloEntreSolicitacaoEInicio = Main.verificarDataSolicitacao(LocalDateTime.now().plusDays(7));
+		assertFalse(intervaloEntreSolicitacaoEInicio);
+	}
+	
+	@Test
+	public void verificaSeIntervaloEntreDataInicioEDataFimFeriasEhMenorQue20Dias() {
+		boolean intervaloEntreSolicitacaoEInicio = Main.verificarIntervaloFeriasParciais(LocalDateTime.of(2021, 3, 29, 0, 0), LocalDateTime.of(2021, 4, 10, 0, 0));
+		assertTrue(intervaloEntreSolicitacaoEInicio);
+	}
+	
+	@Test
+	public void verificaSeIntervaloEntreDataInicioEDataFimFeriasEhMaiorQue20Dias() {
+		boolean intervaloEntreSolicitacaoEInicio = Main.verificarIntervaloFeriasParciais(LocalDateTime.of(2021, 3, 29, 0, 0), LocalDateTime.of(2021, 5, 1, 0, 0));
+		assertFalse(intervaloEntreSolicitacaoEInicio);
+	}
+	
+}
