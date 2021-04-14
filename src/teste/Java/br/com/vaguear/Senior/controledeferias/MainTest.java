f7e3116b2286a5635a78f0fdf488847011a9f2b3
Daniella Lira Moreno <73200886+Danilira@users.noreply.github.com>
2021-04-14 17:38:30 -0300

    Inserindo teste de Status de chamados

--- src/teste/Java/br/com/vaguear/Senior/controledeferias/MainTest.java
+++ src/teste/Java/br/com/vaguear/Senior/controledeferias/MainTest.java
@@ -201,4 +201,17 @@ public class MainTest {
 
 	}
 	// BRUNO END
+//DANI
+	@Test
+	public void testeGetChamados() {
+		StatusDeChamados main = new StatusDeChamados();
+		main.criarChamados();
+		
+		int tipoChamado = 0;
+		String[] retornoChamados = main.getChamados(tipoChamado);
+		assertEquals("chamado 01", retornoChamados[0]);
+		assertEquals("chamado 02", retornoChamados[1]);
+		assertEquals("chamado 03", retornoChamados[2]);		
+
+	}
 }
